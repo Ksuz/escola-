29e25562b299f883e86e0f32a6cbee7fc7fce54f
Ksuz <117034772+Ksuz@users.noreply.github.com>
2023-03-13 09:08:32 -0300

    Add files via upload

---
+++ Kevin B nobre dnv/Aula 06/j.js
@@ -0,0 +1,23 @@
+/* Laços de repetição - FOR */
+
+/* for(let i = 1; i <=100;i++){
+    document.write(i, "<br>")
+} */
+
+/* var num = prompt("Digite um numero: ")
+var num1 = prompt("Digite outro numero: ")
+for(let i = num; i <= num1;i = num++){
+    document.write(i, "<br>")
+} */
+
+/* for(let i = 0; i<=10;i++){
+    if(i % 2==0){
+        document.write(`esse numero é par: (${i}) | e esse é impar (${i+1}) <br>`)
+    }
+} */
+
+/* Exercicio 1 - faça a tabuada */
+for( let i = 2; i<=10; i++){
+    document.write(`${i} + 2 = ${i+2}<br>`)
+
+}
